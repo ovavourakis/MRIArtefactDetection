29f34cbd407cb80909e4f59d2964723853f7d737
Iris Marmouset <irismarmouset@gmail.com>
2024-01-17 21:39:17 +0000

    Name of the modifications fixed

--- inference/DataLoader.py
+++ inference/DataLoader.py
@@ -18,8 +18,8 @@ flip_0_1 = tio.Compose([flip_0, flip_1]) # 1st & 2nd dimension
 flip_0_2 = tio.Compose([flip_0, flip_2]) # 1st & 3nd dimension
 flip_1_2 = tio.Compose([flip_1, flip_2]) # 2nd & 3rd dimension
 flip_0_1_2 = tio.Compose([flip_0, flip_1, flip_2]) # 1st & 2nd & 3rd dimension
-
-augmentations = [flip_0, flip_1, flip_2, flip_0_1, flip_0_2, flip_1_2, flip_0_1_2]
+augmentations = {"flip_0":flip_0, "flip_1":flip_1, "flip_2":flip_2,"flip_0_1":flip_0_1, 
+                 "flip_0_2":flip_0_2, "flip_1_2":flip_1_2, "flip_0_1_2":flip_0_1_2}
 
 # TorchIO function to generate artefacted images
 add_bias = tio.RandomBiasField() # Magnetic field inhomogeneities in the MRI scanner produce low-frequency intensity distortions in the images
@@ -27,9 +27,10 @@ add_spike = tio.RandomSpike() # signal peak in  𝑘 -space,
 add_ghosts = tio.RandomGhosting() # removing every  𝑛 th plane from the k-space
 add_motion = tio.RandomMotion()  # filling the  𝑘 -space with random rigidly-transformed versions of the original images
 random_affine = tio.RandomAffine(scales=(1.5, 1.5)) # zooming in the images
-artefacts = [add_bias, add_spike, add_ghosts, add_motion, random_affine]
+artefacts = {"add_bias":add_bias, "add_spike":add_spike, "add_ghosts":add_ghosts, 
+             "add_motion":add_motion, "random_affine":random_affine}
 
-modifications = augmentations + artefacts
+modifications = {**augmentations, **artefacts}
 
 
 # Looping through all the subfolders in the base directory: 
@@ -52,12 +53,12 @@ for subfolder_name in os.listdir(BASEDIR):
                         modified_image = modification(subject)
 
                         # 1 - Save its ground truth in the dedicated file along with its id
-                        id = f"{subfolder_name}_{modification}"
-                        gt_score = 0 if modification.startswith("flip") else 1
-                        ground_truth_labels.loc[len(ground_truth_labels)] = [id, gt_score] # TODO store ID with temporaryDir
+                        temp_path = os.path.join(TemporaryDir, f"{subfolder_name}_{modification}_T1W.nii.gz")
+                        id = temp_path
+                        gt_score = 0 if str(modification).startswith("flip") else 1
+                        ground_truth_labels.loc[len(ground_truth_labels)] = [id, gt_score] 
 
                         # 2 - Temporarly save the image so it can be re-loaded with nib library
-                        temp_path = os.path.join(TemporaryDir, f"{id}_T1W.nii.gz")
                         modified_image.mri.save(temp_path)
 
                         # 3 - Run the inference on it
