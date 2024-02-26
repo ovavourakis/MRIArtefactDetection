a5d5958831acd3a6472c8d90f18fb5213606aa1c
Iris Marmouset <irismarmouset@gmail.com>
2024-02-26 22:27:58 +0000

    added back train and train_utils after the deletion

---
+++ training/train.py
@@ -0,0 +1,137 @@
+import os
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.metrics import AUC
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+from sklearn.model_selection import train_test_split
+
+from train_utils import DataCrawler, DataLoader
+from model import getConvNet
+
+# Check for GPU availability and set TensorFlow to use GPU if available
+physical_devices = tf.config.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    print("Using GPU")
+else:
+    print("Using CPU")
+
+DATADIR = '/Users/odysseasvavourakis/Documents/2023-2024/Studium/SABS/GE Project/data.nosync/struc_data'
+DATASETS = ['artefacts'+str(i) for i in [1,2,3]]
+CONTRASTS = ['T1wMPR'] #, 'T1wTIR', 'T2w', 'T2starw', 'FLAIR']
+QUALS = ['clean', 'exp_artefacts']
+
+ARTEFACT_DISTRO = { # TODO
+    'RandomZoom' : 1/15,
+    #'RandomElasticDeformation' : ,     # not defined
+    'RandomAnisotropy' : 1/15, 
+    'Intensity' : 1/15, 
+    'RandomMotion' : 2/15,              # more common
+    'RandomGhosting' : 2/15,            # more common
+    'RandomSpike' : 1/15,  
+    'RandomBiasField' : 2/15,           # more common
+    'RandomBlur' : 1/15, 
+    #'RandomNoise' : ,                  # not defined
+    'RandomSwap' : 1/15, 
+    'RandomGamma' : 1/15,
+    'RandomWrapAround' : 2/15,          # more common
+}
+
+if __name__ == '__main__':
+   
+    # get the paths and labels of the real images
+    real_image_paths, real_labels = DataCrawler(DATADIR, DATASETS, CONTRASTS, QUALS).crawl()
+
+    # train-val-test split (stratified to preserve class prevalence)
+    Xtrv, Xtest, ytrv, ytest = train_test_split(real_image_paths, real_labels, test_size=0.3, stratify=real_labels)
+    Xtrain, Xval, ytrain, yval = train_test_split(Xtrv, ytrv, test_size=0.3, stratify=ytrv)
+
+    print('train artefact percentage: ', sum(ytrain)/len(ytrain))
+    print('val artefact percentage: ', sum(yval)/len(yval))
+    print('test artefact percentage: ', sum(ytest)/len(ytest))
+
+    # write out real-image-paths in test set and labels to file for full evaluation later
+    with open('test_split_gt.tsv', 'w') as f:
+        for i, path in enumerate(Xtest):
+            f.write(path + '\t' + str(ytest[i]) + '\n')
+
+    # instantiate DataLoaders
+    trainloader = DataLoader(Xtrain, ytrain, train_mode=True,
+                            target_clean_ratio=0.5, artef_distro=ARTEFACT_DISTRO, batch_size=4)
+    valloader = DataLoader(Xval, yval, train_mode=False,
+                            target_clean_ratio=0.5, artef_distro=ARTEFACT_DISTRO, batch_size=4)
+    testloader = DataLoader(Xtest, ytest, train_mode=False,
+                            target_clean_ratio=0.5, artef_distro=ARTEFACT_DISTRO, batch_size=4)
+
+    # compile model
+    model = getConvNet(out_classes=2, input_shape=(256,256,64,1))
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='nadam', 
+                  metrics=['accuracy', AUC(curve='ROC'), AUC(curve='PR')])
+    
+    print(model.summary())
+
+    # prepare for training
+    # make directory to store model checkpoints
+    checkpoint_dir = "./ckpts"
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+
+    # define callbacks
+    callbacks = [
+        # save model after each epoch
+        ModelCheckpoint(filepath=checkpoint_dir + "/end_of_epoch_{epoch}.keras"), 
+        # reduce learning rate if val_loss doesn't improve for 2 epochs
+        ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, mode='auto',
+                          min_delta=1e-2, cooldown=0, min_lr=0.0001),
+        # stop training if val_loss doesn't improve for 5 epochs
+        EarlyStopping(monitor="val_loss", min_delta=1e-2, patience=5, verbose=1)
+    ]
+
+    # train model
+    history = model.fit(trainloader, 
+                        validation_data=valloader,
+                        steps_per_epoch=26,        # 26 batches per epoch
+                        epochs=24,                 # 24 epochs = 624 gradient updates
+                        callbacks=callbacks,
+                        use_multiprocessing=True,
+                        workers=2)
+    training_losses = history.history['loss']
+    validation_losses = history.history['val_loss']
+
+    # write out train and validation losses
+    with open('train_losses.tsv', 'w') as f:
+        for i, loss in enumerate(training_losses):
+            f.write(str(i) + '\t' + str(loss) + '\n')
+    with open('val_losses.tsv', 'w') as f:
+        for i, loss in enumerate(validation_losses):
+            f.write(str(i) + '\t' + str(loss) + '\n')
+    # plot train losses
+    plt.plot(training_losses, label='Training Loss')
+    plt.plot(validation_losses, label='Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title('Training and Validation Losses')
+    plt.legend()
+    plt.savefig('training_losses.png')
+
+    # evaluate model
+    print("#"*30)
+    print("Evaluate on Test Set")
+    print("#"*30)
+    result = model.evaluate(testloader)
+    dict(zip(model.metrics_names, result))
+    
+
+# TODO:
+
+# splitting by patient
+# input size - rasampling or sub-volume sampling or registration
+# make sure you save everything during training (straight to GDrive)
+# tack on probabilsitic inference run on test at end of training
+# evaluate probabilistic inference
+# gradually shift training distribution to real distribution
+# get set up on arc
+
+# define artefact distribution
+# code wrap-around and affine transformation for field gradient ingomoegenity
---
+++ training/train_utils.py
@@ -0,0 +1,298 @@
+import os, random, math
+import numpy as np
+import tensorflow as tf
+import torchio as tio
+from keras.utils import Sequence
+from keras.callbacks import Callback
+
+class ImageReader():
+    '''
+    Class to read and preprocess images from disk.
+    Handles image loading, synthetic augmentation and 
+    preprocessing (resizing, normalisation, padding etc.).
+    '''
+
+    def __init__(self, input_size=(256,256,64)):
+        self.input_size = input_size
+        self.Synths = {
+            "RandomZoom": tio.RandomAffine(scales=(1.5, 1.5)),          # zooming in the images 
+            # "RandomElasticDeformation": tio.RandomElasticDeformation(), # INCORRECT: parameters unchosen; elastic deformation of the images 
+            "RandomAnisotropy": tio.RandomAnisotropy(),                 # anisotropic deformation of the images
+            "RescaleIntensity": tio.RescaleIntensity((0.5, 1.5)),       # rescaling the intensity of the images
+            "RandomMotion": tio.RandomMotion(degrees = (10,15), translation = (10,15)), # filling the  𝑘 -space with random rigidly-transformed versions of the original images
+            "RandomGhosting": tio.RandomGhosting(),                     # removing every  𝑛 th plane from the k-space
+            "RandomSpike": tio.RandomSpike(),                           # signal peak in  𝑘 -space,
+            "RandomBiasField": tio.RandomBiasField(coefficients = (0.3,0.6)), # Magnetic field inhomogeneities in the MRI scanner produce low-frequency intensity distortions in the images
+            "RandomBlur": tio.RandomBlur(std = (0.6,1.1)),              # blurring the images
+            # "RandomNoise": tio.RandomNoise(),                         # INCORRECT: does not work (why ?); adding noise to the images
+            "RandomSwap": tio.RandomSwap(),                             # swapping the phase and magnitude of the images
+            "RandomGamma": tio.RandomGamma(log_gamma = (0.25,0.35)),    # intensity of the images
+            "RandomWrapAround" : self.RandomWrapAround                     # wrapping around the images: Aliasing Artifact
+        }
+        # pre-processing
+        self.orient = tio.transforms.ToCanonical()                  # RAS+ orientation
+        self.normalise = tio.transforms.ZNormalization()            # TODO: consider histogram normalisation instead
+        self.crop_pad = tio.transforms.CropOrPad(self.input_size)   # TODO: consider re-sampling instead, or in addition
+        self.preprocess = tio.Compose([self.orient, self.normalise, self.crop_pad])
+
+    def RandomWrapAround(self, subject):
+        """ Wrapping around the images: Aliasing Artifact """
+        modified = subject
+        dim_value = np.random.choice([0, 1, 2]) # Pick the dimension to wrap on
+        side = np.random.choice([0, 1]) # Pick the side we want to add the wrap-around
+        nb_rows = random.randint(subject.shape[dim_value+1]//4, subject.shape[dim_value+1]//3) 
+        if side == 0: # wrap the last rows to the firsts rows
+                if dim_value == 0: modified.data[:, :nb_rows, :, :] += subject.data[:,-nb_rows:, :, :] 
+                elif dim_value == 1: modified.data[:, :, :nb_rows, :] += subject.data[:, :,-nb_rows:, :] 
+                elif dim_value == 2: modified.data[:, :, :, :nb_rows] += subject.data[:, :, :,-nb_rows:]
+        elif side == 1: # wrap the firsts rows to the last rows
+                if dim_value == 0: modified.data[:,-nb_rows:, :, :] += subject.data[:, :nb_rows, :, :] 
+                elif dim_value == 1: modified.data[:, :,-nb_rows:, :] += subject.data[:, :, :nb_rows, :] 
+                elif dim_value == 2: modified.data[:, :, :, -nb_rows:] += subject.data[:, :, :, :nb_rows]
+        return modified
+ 
+
+    def _apply_modifications(self, img_path):
+        # 1 - Checking the extension on the img_path + storing it as extension_name (i.e the modification to apply)
+        extensions = ["CAug", "RandomZoom", 
+          'RandomAnisotropy', 'RescaleIntensity', 'RandomMotion', 'RandomGhosting', 'RandomSpike', 
+          'RandomBiasField', 'RandomBlur', 'RandomSwap', 'RandomGamma', 'RandomWrapAround']
+        extension_name = None 
+        for extension in extensions:
+            if extension in img_path:
+                extension_name = extension
+                break
+        
+         # 2 - Creating a torchIO image from the path
+        if extension_name is None: # no modification to be applied
+            img = tio.ScalarImage(img_path)
+            return img
+        stripped_img_path = img_path.replace(f"_{extension_name}.nii", ".nii")   
+        img = tio.ScalarImage(stripped_img_path)
+        
+        # 3 - Defining the modification from extension_name
+        if extension_name == "CAug":
+            binary_flip = [np.random.choice([0, 1]) for _ in range(3)]
+            idx_flip = [index for index, value in enumerate(binary_flip) if value == 1]
+            flipping = tio.RandomFlip(axes=idx_flip, flip_probability=1)
+            scaling = tio.RandomAffine(scales=(0.1)) # If only one value: U(1-x, 1+x)
+            shifting = tio.RandomAffine(translation=(0.1)) # If only one value: U(-x, x)
+            rotating = tio.RandomAffine(degrees=(10)) # If only one value: U(-x, x) 
+            modification = tio.Compose([flipping, scaling, shifting, rotating])
+
+        elif extension_name in ["RandomZoom", 
+                                'RandomAnisotropy', 'RescaleIntensity', 
+                                'RandomMotion', 'RandomGhosting', 'RandomSpike', 
+                                'RandomBiasField', 'RandomBlur', 
+                                'RandomSwap', 'RandomGamma', 'RandomWrapAround']:
+            modification = self.Synths[extension_name]
+        
+         # 4 - Apply the modification on the modified image and return modified version
+        modified_img = modification(img)   
+        # modified_img.save(img_path)
+        return modified_img
+
+    def read_image(self, path):
+        img = self._apply_modifications(path) # introduce artefacts, if specified in path_name
+        img = self.preprocess(img) # re-orient, normalise, crop/pad
+        return img # numpy array
+
+class DataCrawler():
+    """
+    Crawls the dataset directories and returns a list of clean/artefact 
+    image paths with labels.
+    """
+
+    def __init__(self, datadir, datasets, image_contrasts, image_qualities):
+        self._check_inputs(datadir, datasets, image_contrasts, image_qualities)
+        self.datadir = datadir
+        self.datasets = datasets
+        self.image_contrasts = image_contrasts
+        self.image_qualities = image_qualities
+       
+    def _check_inputs(self, datadir, datasets, image_contrasts, image_qualities):
+        # do paths to all requested types of images exist?
+        assert(os.path.isdir(datadir))
+        for d in datasets:
+            assert(os.path.isdir(os.path.join(datadir, d)))
+            for c in image_contrasts:
+                assert(c in ['T1wMPR', 'T1wTIR', 'T2w', 'T2starw', 'FLAIR'])
+                if os.path.isdir(os.path.join(datadir, c)):
+                    for q in image_qualities:
+                        assert(q in ['clean', 'exp_artefacts'])
+                        assert(os.path.isdir(os.path.join(datadir, c, q)))
+
+    def crawl(self):
+        # get all the (non-synthetic) image paths
+        clean_img_paths = []
+        artefacts_img_paths = []
+        for d in self.datasets:
+            for c in self.image_contrasts:
+                for q in self.image_qualities:
+                    path = os.path.join(self.datadir, d, c, q)
+                    images = os.listdir(path)
+                    img_paths = [os.path.join(path, i) for i in images]
+                    if q == 'clean':
+                        clean_img_paths.extend(img_paths)
+                    else:
+                        artefacts_img_paths.extend(img_paths)
+        # define the appropriate labels
+        num_clean = len(clean_img_paths)
+        num_artefacts = len(artefacts_img_paths)
+        y_true = np.array([1]*num_clean + [0]*num_artefacts)
+
+        return clean_img_paths + artefacts_img_paths, y_true
+
+class DataLoader(Sequence):
+    """
+    Dataloader for an image dataset.
+    Pre-defines augmentations to be performed in order to reach a target clean-ratio.
+    """
+
+    def __init__(self, Xpaths, y_true, batch_size, target_clean_ratio, artef_distro, train_mode=True):
+        self._check_inputs(Xpaths, y_true, artef_distro, target_clean_ratio)
+
+        self.batch_size = batch_size
+        self.num_classes = len(np.unique(y_true))
+        self.reader = ImageReader(input_size=(256,256,64))
+        
+        # paths to just the *real* images
+        clean_idx, artefact_idx = np.where(y_true == 1)[0], np.where(y_true == 0)[0]
+        self.Clean_img_paths = [Xpaths[i] for i in clean_idx]
+        self.Artefacts_img_paths = [Xpaths[i] for i in artefact_idx]
+        self.clean_ratio = self._get_clean_ratio()
+
+        self.train_mode = train_mode
+        if self.train_mode:
+            self.target_clean_ratio = target_clean_ratio
+            self.target_artef_distro = artef_distro
+
+        # paths to all images, including synthetic ones; split into batches
+        self.clean_img_paths, self.artefacts_img_paths, self.batches, self.labels = self.on_epoch_end()
+
+    def _check_inputs(self, Xpaths, y_true, artef_distro, target_clean_ratio):
+        assert(len(Xpaths) == len(y_true))
+        assert(target_clean_ratio >= 0 and target_clean_ratio <= 1)
+        # artef_distro defines a categorical probability distribution over (synthetic) artefact types
+        assert(isinstance(artef_distro, dict))
+        assert(sum(artef_distro.values()) == 1)
+        for k, v in artef_distro.items():
+            assert(v >= 0)
+            assert(k in ['RandomZoom', 
+                         'RandomAnisotropy', 'Intensity', 'RandomMotion', 'RandomGhosting', 
+                         'RandomSpike', 'RandomBiasField', 'RandomBlur', 
+                         'RandomSwap', 'RandomGamma', 'RandomWrapAround'])
+
+    def _get_clean_ratio(self):
+        # calculate fraction of clean images (among non-synthetic data)
+        C = len(self.Clean_img_paths)
+        T = C + len(self.Artefacts_img_paths)
+        return C/T, C, T
+    
+    def _define_augmentations(self):
+        '''Generates pathnames for synthetic images to be created in order to reach target clean-ratio.
+        Synthetic clean images defined by random {flips, shifts, scales, rotations}.
+        Synthetic artefact images defined by transforms drawn from a categorical distro over artefact types.'''
+        
+        def _pick_augment(path, aug_type='clean'):
+            if aug_type=='clean':
+                aug = '_CAug'
+            elif aug_type=='artefact':
+                augs = list(self.target_artef_distro.keys())
+                probs = list(self.target_artef_distro.values())
+                aug = np.random.choice(augs, size = 1, p = probs)
+            else:
+                raise ValueError('aug_type must be either "clean" or "artefact"')
+            dir, file = os.path.split(path)
+            fname, ext = file.split('.', 1) # just the first dot, so we don't split extensions like .nii.gz
+            return os.path.join(dir, fname) + aug + '.' + ext
+        
+        clean_ratio, cleans, total = self._get_clean_ratio()
+        clean_img_paths = self.Clean_img_paths
+        artefacts_img_paths = self.Artefacts_img_paths
+    
+        if clean_ratio < self.target_clean_ratio:
+            # oversample clean images with random {flips, shifts, 10% scales, rotations} 
+            # until desired clean-ratio is reached
+            num_imgs_to_aug = int( (total*self.target_clean_ratio - cleans) / (1-self.target_clean_ratio))
+            imgs_to_aug = random.choices(clean_img_paths, k=num_imgs_to_aug)
+            augmented_paths = [_pick_augment(path, aug_type='clean') for path in imgs_to_aug]
+            clean_img_paths.extend(augmented_paths)
+        elif clean_ratio > self.target_clean_ratio:
+            # create synthetic artefacts until desired clean-ratio is reached
+            # pick aigmentations from categorcical distro over transform functions of TorchIO
+            num_imgs_to_aug = cleans/self.target_clean_ratio - total
+            imgs_to_aug = random.choices(clean_img_paths, k=num_imgs_to_aug)
+            augmented_paths = [_pick_augment(path, aug_type='artefact') for path in imgs_to_aug]
+            artefacts_img_paths.extend(augmented_paths)
+        
+        return clean_img_paths, artefacts_img_paths
+
+    def __len__(self):
+        return math.ceil((len(self.clean_img_paths)+len(self.artefacts_img_paths)) // self.batch_size)
+
+    def _def_batches(self, clean_img_paths, artefacts_img_paths):
+        """ Determine batches at start of each epoch:
+        Assign to batches the paths with the repartition we target
+        Makes sure target clean-ratio is maintained in each batch
+        Makes sure all the existing images are used in the epoch
+        """
+
+        # 1 - Decide on number of clean and artefact images in each batch with the repartition we target
+        clean_ratio = self.target_clean_ratio if self.train_mode else self.clean_ratio
+        num_clean = int(self.batch_size * clean_ratio)
+
+        num_artefacts = self.batch_size - num_clean
+
+        # 2 - Randomly assign the paths to the batches along with their labels
+        nb_batches = len(clean_img_paths + artefacts_img_paths) // self.batch_size
+        random.shuffle(self.clean_img_paths)
+        random.shuffle(self.artefacts_img_paths)
+        batches = [
+            self.clean_img_paths[i * num_clean:(i + 1) * num_clean] + self.artefacts_img_paths[i * num_artefacts:(i + 1) * num_artefacts]
+            for i in range(nb_batches)
+            ]
+        labels = [[1]*num_clean + [0]*num_artefacts for _ in range(nb_batches)]
+
+        # 3 - Shuffle batch in batches ALONG with their labels
+        for i in range(nb_batches):
+            zipped = list(zip(batches[i], labels[i]))
+            random.shuffle(zipped)
+            batches[i], labels[i] = zip(*zipped)
+            batches[i] = list(batches[i])
+            labels[i] = list(labels[i])
+
+        return batches, labels
+
+    def __getitem__(self,idx):
+        '''Generates the batch, with associated labels.'''
+        # read in the images, augment with artefacts as neccessary, then apply pre-processing
+        batch_images = [self.reader.read_image(path) for path in self.batches[idx]]
+        X = np.stack([np.squeeze(img.data.numpy()) for img in batch_images], axis = 0)
+        
+        # also get appropriate labels
+        y_true = self.labels[idx]
+        y_one_hot = tf.one_hot(y_true, depth=self.num_classes)
+
+        return X, y_one_hot
+    
+    def on_epoch_end(self):
+        # re-define augmentations to do for next epoch
+        # get paths to all images, including synthetic ones, if requested
+        if self.train_mode:
+            self.clean_img_paths, self.artefacts_img_paths = self._define_augmentations()
+        else:
+            self.clean_img_paths, self.artefacts_img_paths = self.Clean_img_paths, self.Artefacts_img_paths
+        # split these new image paths into new random batches
+        self.batches, self.labels = self._def_batches(self.clean_img_paths, self.artefacts_img_paths)
+
+        return self.clean_img_paths, self.artefacts_img_paths, self.batches, self.labels
+
+class FullLossHistory(Callback):
+    '''do not use! logs cumulative average of epoch'''
+
+    def on_train_begin(self, logs):
+        self.per_batch_losses = []
+
+    def on_batch_end(self, batch, logs):
+        self.per_batch_losses.append(logs.get("loss"))
